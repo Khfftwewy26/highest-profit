187a4d72fab89ba2551ebb5da5b077e5dcf9f712
VanessaWhite26 <46722056+VanessaWhite26@users.noreply.github.com>
2021-10-02 13:55:25 -0400

    Finished Part 1 of the prompt

--- main.py
+++ main.py
@@ -7,8 +7,30 @@ def readData(filename):
         rows = len(list(readProfit))
         print(rows)
 
+def readValidData(filename):
+    finalData = []
+    with open(filename) as profitInfo:
+        readProfit = csv.reader(profitInfo, delimiter = ',')
+        for row in readProfit:
+            if row[4].isdigit():
+                finalData.append(row[4])
+            else:
+                try:
+                    float(row[4])
+                    finalData.append(row[4])
+                except ValueError:
+                    continue
+
+        validData = len(list(finalData))
+        print(validData)
+
+
+
+
+
 
 # Calls the read function
 if __name__ == '__main__':
     readData('profitData.csv')
+    readValidData('profitData.csv')
 
