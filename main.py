f48a5fb202fb9f3583fd5e81576e61f9fc4c92db
VanessaWhite26 <46722056+VanessaWhite26@users.noreply.github.com>
2021-10-03 16:57:29 -0400

    Finished placing data into json file

--- main.py
+++ main.py
@@ -1,36 +1,60 @@
-import csv
+import csv, json
+
+jsonFilePath = r'data2.json'
+csvFilePath = r'profitData.csv'
 
 #Takes the filename in the function and reads/prints the csv
 def readData(filename):
     with open(filename) as profitInfo:
-        readProfit = csv.reader(profitInfo, delimiter = ',')
+        readProfit = csv.DictReader(profitInfo, delimiter = ',')
         rows = len(list(readProfit))
         print(rows)
 
+
 def readValidData(filename):
-    finalData = []
+    finalValidList = []
     with open(filename) as profitInfo:
         readProfit = csv.reader(profitInfo, delimiter = ',')
         for row in readProfit:
             if row[4].isdigit():
-                finalData.append(row[4])
+                finalValidList.append(row)
             else:
                 try:
                     float(row[4])
-                    finalData.append(row[4])
+                    finalValidList.append(row)
                 except ValueError:
                     continue
 
-        validData = len(list(finalData))
+        validData = len(list(finalValidList))
         print(validData)
 
+        finalValidList.sort(key = lambda x: (float(x[4])), reverse= True)
 
 
+def createJSON(validFinalData, jsonFilePath):
+    data = []
 
+    with open(validFinalData) as csvFile:
+        csvRead = csv.DictReader(csvFile)
+        for rows in csvRead:
+            if rows['Profit (in millions)'].isdigit():
+                data.append(rows)
+            else:
+                try:
+                    float(rows['Profit (in millions)'])
+                    data.append(rows)
+                except ValueError:
+                    continue
+
+        data.sort(key = lambda x: float(x['Profit (in millions)']), reverse = True)
+
+    with open(jsonFilePath, 'w',encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
 
 
 # Calls the read function
 if __name__ == '__main__':
     readData('profitData.csv')
     readValidData('profitData.csv')
+    createJSON('profitData.csv','data2.json')
 
